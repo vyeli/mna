39029deb12f53c6fc9e5f8f9dd7d5aadd83776d2
vyeli <yeliv9527@gmail.com>
2022-12-21 23:46:19 -0300

    xd

--- part2/parciales/IVPMULTNODO.m
+++ part2/parciales/IVPMULTNODO.m
@@ -1,31 +1,14 @@
-#Sistema para IVP, aproximar la derivada con varios puntos 
+#Sistema para IVP, aproximar la derivada con varios puntos
 
 #Rec Ej4
 
 A=[1 1 1 1 1; -1 0 1 2 3; 1/2 0 1/2 2 9/2; -1/6 0 1/6 4/3 9/2; 1/24 0 1/24 2/3 27/8];
-B=[0 1 0 0 0]';
+B=[0 0 1 0 0]';
 
-Resp = A\B;
-rats(Resp);  #Formato racional
-
-f= @(x) 3*e^(3*x);
-
-h=0.1;
-format long;
-aprox = 1/(12*h) * (-3*e^(3*(3-h))-10*e^(3*3)+18*e^(3*(3+h))-6*e^(3*(3+2*h))+e^(3*(3+3*h)));
-
-error1 = abs(f(3)-aprox);
-
-h=0.05;
-aprox = 1/(12*h) * (-3*e^(3*(3-h))-10*e^(3*3)+18*e^(3*(3+h))-6*e^(3*(3+2*h))+e^(3*(3+3*h)));
-error2 = abs(f(3)-aprox);
-
-xd = error1/error2;
-
-
-#EJ4 parcial 2021
-
-A=[1 1 1 1 1; 2 1 0 -1 -2; 2 1/2 0 1/2 2; 4/3 1/6 0 -1/6 -4/3; 2/3 1/24 0 1/24 2/3];
-B=[0 0 0 1 0]';
 Resp = A\B;
 rats(Resp)  #Formato racional
+
+
+format long
+xd = 1/12*(11*0.045242-20*0.05+6*0.045242+4*0.033516-0.020328)
+
---
+++ part2/parciales/ej7recu2_2022.m
@@ -0,0 +1,22 @@
+clear all ; close all; clc
+
+x0 = 0;
+xf = 1;
+
+NI = 5;       # Nodos internos
+h = (xf-x0)/(NI+1);  # 1/40
+
+diagSup = ones(NI, 1)*(1+h);
+diagInf = ones(NI, 1)*(1-h);
+diag = ones(NI, 1)*(-2);
+
+M = spdiags([diagInf diag diagSup], -1:1, NI, NI);
+b = [-10*h.^3-1+h];
+
+for i=2:NI
+  b(i,1) = -10*i*h.^3;
+endfor
+
+Faprox = M\b;
+
+Faprox((NI+1)./2)
